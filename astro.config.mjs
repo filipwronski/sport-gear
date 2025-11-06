e26ed13847846e78a41ccd1a7a865dd573a92efd
Filip Wronski <filip.wronski@morizon-gratka.pl>
2025-11-06 21:32:16 +0100

    fix: vercel adapter configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,14 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
-import node from "@astrojs/node";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [react(), sitemap()],
-  server: { port: 3000 },
   vite: {
     plugins: [tailwindcss()],
   },
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: vercel(),
 });
